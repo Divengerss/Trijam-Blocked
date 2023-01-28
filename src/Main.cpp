96cd8342e8ed04425457a1abbad5ed5d46b1ea4c
Divengerss <dorvann.ledeux@epitech.eu>
2023-01-28 19:27:15 +0100

    feat: Texts on the window
    Changes to be committed:
    	new file:   BlockMeThat
    	modified:   Makefile
    	modified:   src/Main.cpp
    	new file:   src/Text.cpp
    	new file:   src/Text.hpp
    	modified:   src/Window.cpp
    	modified:   src/Window.hpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,10 @@
 int main(int argc, const char *argv[])
 {
     bmt::Windows gameWin("BlockMeThat", 800, 600);
+    sf::Font font;
+    if (!font.loadFromFile("./assets/font.ttf"))
+        return EXIT_FAILURE;
+    sf::Text text("Hello SFML", font, 50);
 
     while (gameWin.isOpen()) {
         sf::Event event;
@@ -11,6 +15,7 @@ int main(int argc, const char *argv[])
                 gameWin.close();
         }
         gameWin.clear();
+        gameWin.getWindow().draw(text);
         gameWin.display();
     }
     return 0;
--- src/Window.hpp
+++ src/Window.hpp
@@ -9,6 +9,7 @@
 #include <tuple>
 
 #include "Event.hpp"
+#include "Text.hpp"
 
 namespace bmt
 {
@@ -16,26 +17,36 @@ namespace bmt
     public:
         Windows(const std::string &title, const std::size_t &x, const std::size_t &y);
         Windows(const std::string &title, const std::tuple<std::size_t, std::size_t> &size);
-        ~Windows() = default;
+        ~Windows();
 
         void setTitle(const std::string &title);
         void setSize(const std::size_t &x, const std::size_t &y);
         void setSize(const std::tuple<std::size_t, std::size_t> &size);
 
+        void setNewText(const std::string &str);
+        void setNewText(const std::string &str, const std::size_t &charSize);
+        void setNewText(const std::string &str, const std::size_t &charSize, const sf::Color &col);
+        void setNewText(const std::string &str, const std::size_t &charSize, const sf::Color &col, const std::string &fontPath);
+        void setNewText(const std::string &str, const std::size_t &charSize, const std::string &fontPath);
+
         std::string &getTitle();
         std::tuple<std::size_t, std::size_t> &getSize();
         Event &getEvent();
+        sf::RenderWindow &getWindow();
 
         bool pollEvent();
         bool isOpen();
         void close();
         void clear();
         void display();
+
+        void drawTexts();
     private:
         sf::RenderWindow _win;
         std::string _title;
         std::tuple<std::size_t, std::size_t> _size;
         Event _event;
+        std::vector<Text> _texts;
     };
 }
 
